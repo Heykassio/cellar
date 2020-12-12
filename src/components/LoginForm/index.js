5c8a600479212ae84b41c9bf24035948b4eb5af3
Kassio Costa <heykassio@live.com>
2020-12-12 00:05:22 -0300

    configurando a url

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
+import { baseUrl } from '../../../config';
 import { LoginForm } from './styled';
 
 
@@ -22,7 +23,7 @@ export default function Form() {
         const password = e.target.querySelector('#password').value;
 
         try {
-            const response = await fetch('https://api-arca.heykassio.com/login', {
+            const response = await fetch(`${baseUrl}/login`, {
             credentials: 'include',
             method: 'POST',
             headers: {
